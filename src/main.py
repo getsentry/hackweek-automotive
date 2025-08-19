1d36953c37d4d4df57fc3997047250163dc2b4b6
Jan Michael Auer <mail@jauer.org>
2025-08-19 10:25:43 +0200

    Hello world main file

---
+++ src/main.py
@@ -0,0 +1,15 @@
+"""
+Sentry CarBuddy - OBD-II Vehicle Diagnostics to Sentry Integration
+
+A headless Raspberry Pi application that reads Diagnostic Trouble Codes (DTCs)
+from vehicles via a factory-paired Bluetooth OBD-II adapter and reports them
+to Sentry.io for monitoring and alerting.
+"""
+
+
+def main():
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
